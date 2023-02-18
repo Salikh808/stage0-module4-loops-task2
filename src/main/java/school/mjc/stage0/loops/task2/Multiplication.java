edaf593d56de9ecc2cf698e6469471d74e787f97
nurzhan <Salikh835@gmail.com>
2023-02-18 16:15:36 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int i = 0, res;
+
+        while (i<=multiplyByAndToInclusive){
+            res = i*multiplyByAndToInclusive;
+            System.out.println(res);
+            i++;
+        }
     }
 }
